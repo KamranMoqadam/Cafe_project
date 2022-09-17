2970809970381c0da7b64dd8beabe1e8078f8d00
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:45:58 +0430

    updated

--- back_end/model/order/utils.py
+++ back_end/model/order/utils.py
@@ -1,29 +1,29 @@
-from back_end.model.order import oredr_model
-from ..users.users_model import users
-from ..item.item_model import item
-from ..tables.table_model import tables
-from ..log import cafe_log
+from back_end.model.users.users_model import Users
+from back_end.model.item.item_model import Menu_Item
+from back_end.model.tables.table_model import tables
+from back_end.model.log import cafe_log
+
 
 class Validator:
-    valid_order: oredr_model.orders
+    valid_order: any
 
     @classmethod
     def validate(cls):
-        if isinstance(cls.valid_order.takeaway, bool):
+        if not isinstance(cls.valid_order.takeaway, bool):
             cafe_log.error("invalid take away")
             raise ValueError("invalid take away")
-        if isinstance(cls.valid_order.number, int):
+        if not isinstance(cls.valid_order.number, int):
             cafe_log.error("invalid take number")
             raise ValueError("invalid number")
-        if isinstance(cls.valid_order.status, str):
+        if not isinstance(cls.valid_order.status, str):
             cafe_log.error("invalid take status")
             raise ValueError("invalid status")
-        if isinstance(cls.valid_order.user_id, users):
+        if not isinstance(cls.valid_order.user_id, Users):
             cafe_log.error("invalid take user id")
             raise ValueError("invalid user id")
-        if isinstance(cls.valid_order.item_id, item):
+        if not isinstance(cls.valid_order.item_id, Menu_Item):
             cafe_log.error("invalid take item id")
             raise ValueError("invalid take item id")
-        if isinstance(cls.valid_order.table_id, tables):
+        if not isinstance(cls.valid_order.table_id, tables):
             cafe_log.error("invalid take table id")
             raise ValueError("invalid table id")
