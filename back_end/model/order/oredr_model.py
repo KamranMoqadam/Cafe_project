d9cbde3a2c02efc8ed8a3f936b7cc48afe74db8d
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:44:42 +0430

    updated

--- back_end/model/order/oredr_model.py
+++ back_end/model/order/oredr_model.py
@@ -2,30 +2,18 @@ import datetime
 from peewee import *
 import back_end.core.db_connection as database_connection
 # from ..users.users_model import users_model
-import back_end.model.order.utils as val
-
-db = PostgresqlDatabase('postgres', host='gina.iran.liara.ir', port=33704, user='root',
-                        password='nhmlvrFkHKBA5jxjb9rycf1w')
-
-
-class users(Model):
-    id: int
-
-
-class tables(Model):
-    id: int
-
-
-class item(Model):
-    id: int
+from back_end.model.order.utils import Validator
+from back_end.model.tables.table_model import tables
+from back_end.model.users.users_model import Users
+from back_end.model.item.item_model import Menu_Item
 
 
 class orders(Model):
     order_id = AutoField()
-    user_id = ForeignKeyField(users, on_delete='CASCADE', on_update='CASCADE', backref='orders')
+    user_id = ForeignKeyField(Users, on_delete='CASCADE', on_update='CASCADE', backref='orders')
     table_id = ForeignKeyField(tables, on_delete='CASCADE', on_update='CASCADE', backref='tables')
-    item_id = ForeignKeyField(item, on_delete='CASCADE', on_update='CASCADE', backref='items')
-    number = IntegerField()
+    item_id = ForeignKeyField(Menu_Item, on_delete='CASCADE', on_update='CASCADE', backref='items')
+    number = IntegerField(unique=True)
     status = CharField()
     takeaway = BooleanField()
     order_date = TimestampField(default=datetime.datetime.now())
@@ -36,12 +24,11 @@ class orders(Model):
 
     def save_orders(self):
         with database_connection.db_conn.db:
-            print(self)
             if not self.table_exists():
                 database_connection.db_conn.db.create_tables([self])
 
-            val.Validator.valid_order = self
-            val.Validator.validate()
+            Validator.valid_order = self
+            Validator.validate()
             self.save()
             return 'ok'
 
