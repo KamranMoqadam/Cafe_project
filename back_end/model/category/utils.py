48af2fdfdc045ebcc4f99703b3fd50a66a1289e8
kamranmoqadam <kamranmoqadam97@gmail.com>
2022-09-17 19:14:06 +0430

    Make model category

---
+++ back_end/model/category/utils.py
@@ -0,0 +1,18 @@
+import category_model
+from back_end.log.log import cafe_log
+
+
+class Validator:
+    valid_category: category_model.Category
+
+    @staticmethod
+    def validate(cls):
+        if isinstance(cls.valid_category.category_name, str):
+            cafe_log.error("invalid category name")
+            raise ValueError("invalid category name")
+        if isinstance(cls.valid_category.meal, str):
+            cafe_log.error("invalid meal")
+            raise ValueError("invalid meal")
+        if isinstance(cls.valid_category.category_id, category_model.Category):
+            cafe_log.error("invalid category id")
+            raise ValueError("invalid category id")
