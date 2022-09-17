2d98cde55e2aeb67f061e4d128317e7d68312a41
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:45:29 +0430

    updated

--- back_end/model/tables/table_model.py
+++ back_end/model/tables/table_model.py
@@ -1,31 +1,28 @@
 from peewee import *
 import back_end.core.db_connection as database_connection
-import back_end.model.tables.utils as val
+from back_end.model.tables.utils import Validator
 
-
-db = PostgresqlDatabase('postgres', host='gina.iran.liara.ir', port=33704, user='root',
-                        password='nhmlvrFkHKBA5jxjb9rycf1w')
 
 class tables(Model):
     table_id = AutoField()
     table_number = IntegerField()
     capacity = IntegerField()
     booking = BooleanField()
-    
+
     class Meta:
         database = database_connection.db_conn.db
         db_table = 'tables'
-        
-    def save_orders(self):
+
+    def save_tables(self):
         with database_connection.db_conn.db:
-            print(self)
             if not self.table_exists():
                 database_connection.db_conn.db.create_tables([self])
 
-            val.Validator.valid_table = self
-            val.Validator.validate()
+            Validator.valid_table = self
+            Validator.validate()
             self.save()
             return 'ok'
 
 
-
+tab = tables(table_number=20, capacity=5, booking=False)
+tab.save_tables()
