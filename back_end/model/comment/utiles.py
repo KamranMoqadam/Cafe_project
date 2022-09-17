7718f1215b908604138327dd35c94a58a0e47c95
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:45:43 +0430

    updated

--- back_end/model/comment/utiles.py
+++ back_end/model/comment/utiles.py
@@ -1,53 +1,55 @@
-from back_end.model.comment import comment_model
-from ..users.users_model import users
-from ..item.item_model import item
-from ..log import cafe_log
+
+from back_end.model.users.users_model import Users
+from back_end.model.item.item_model import Menu_Item
+from back_end.model.log import cafe_log
 import re
 
 
 class Validator:
-    valid_comment: comment_model.comment
+    valid_comment: any
     comment_text_regex = re.compile(r"\.{1,}")
+
     @classmethod
     def validate(cls):
-        if Validator.validate_userid(cls.valid_comment.user_id) != True:
+        if not Validator.validate_userid(cls.valid_comment.user_id):
             cafe_log.error("invalid user id")
             raise ValueError("invalid user id")
-        if Validator.validate_menu_id(cls.valid_comment.menu_id) != True:
+        if not Validator.validate_menu_id(cls.valid_comment.menu_id):
             cafe_log.error("invalid menu id")
             raise ValueError("invalid menu id")
-        if Validator.validate_comment_text(cls.valid_comment.comment_text) != True:
+        if not Validator.validate_comment_text(cls.valid_comment.comment_text):
             cafe_log.error("invalid comment text")
             raise ValueError("invalid comment text")
-        if Validator.validate_comment_rate(cls.valid_comment.rate) != True:
+        if not Validator.validate_comment_rate(cls.valid_comment.rate):
             cafe_log.error("invalid comment rate")
             raise ValueError("invalid comment rate")
 
     @staticmethod
     def validate_userid(user_id):
-        if isinstance(user_id, users):
+        if isinstance(user_id, Users):
             return True
         else:
             return False
 
     @staticmethod
     def validate_menu_id(menu_id):
-        if isinstance(menu_id, item):
+        if isinstance(menu_id, Menu_Item):
             return True
         else:
             return False
 
     @staticmethod
     def validate_comment_text(comment_text):
-        if Validator.comment_text_regex.match(comment_text):
+
+        if len(comment_text)>0:
             return True
         else:
             return False
 
     @staticmethod
     def validate_comment_rate(comment_rate):
-        if isinstance(comment_rate,int):
-            if 0<=comment_rate<=10:
+        if isinstance(comment_rate, int):
+            if 0 <= comment_rate <= 10:
                 return True
             else:
                 return False
