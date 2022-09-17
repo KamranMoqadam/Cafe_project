8d7302b32ca47f81404ae56fdc790ddf0aff51ae
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:46:12 +0430

    updated

--- back_end/model/tables/utils.py
+++ back_end/model/tables/utils.py
@@ -1,19 +1,18 @@
-from back_end.model.tables import table_model
-from table_model import tables
-from ..log import cafe_log
+
+from back_end.model.log import cafe_log
+
 
 class Validator:
-    valid_table: table_model.tables
+    valid_table: any
 
     @classmethod
     def validate(cls):
-        if isinstance(cls.valid_table.table_number, int):
+        if not isinstance(cls.valid_table.table_number, int):
             cafe_log.error("invalid take table number")
             raise ValueError("invalid table number")
-        if isinstance(cls.valid_table.capacity, int):
+        if not isinstance(cls.valid_table.capacity, int):
             cafe_log.error("invalid take capcity")
             raise ValueError("invalid capacity")
-        if isinstance(cls.valid_table.booking, bool):
+        if not isinstance(cls.valid_table.booking, bool):
             cafe_log.error("invalid take booking")
             raise ValueError("invalid booking")
-        
