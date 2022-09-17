549d53caff705328573eb01020e63f711866a3e4
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:44:21 +0430

    updated

--- back_end/model/comment/comment_model.py
+++ back_end/model/comment/comment_model.py
@@ -1,27 +1,15 @@
 import datetime
 from peewee import *
 import back_end.core.db_connection as database_connection
-# from ..users.users_model import users_model
-# from ..item.item_model import item_model
+from back_end.model.users.users_model import Users
+from back_end.model.item.item_model import Menu_Item
 import back_end.model.comment.utiles as val
 
-db = PostgresqlDatabase('postgres', host='gina.iran.liara.ir', port=33704, user='root',
-                        password='nhmlvrFkHKBA5jxjb9rycf1w')
 
-
-class users(Model):
-    id: int
-
-
-class item(Model):
-    id: int
-
-
-
-class comment(Model):
+class Comment(Model):
     comment_id = AutoField()
-    user_id = ForeignKeyField(users, on_delete='CASCADE', on_update='CASCADE', backref='orders')
-    menu_id = ForeignKeyField(item, on_delete='CASCADE', on_update='CASCADE', backref='tables')
+    user_id = ForeignKeyField(Users, on_delete='CASCADE', on_update='CASCADE', backref='orders')
+    menu_id = ForeignKeyField(Menu_Item, on_delete='CASCADE', on_update='CASCADE', backref='tables')
     comment_text = CharField()
     rate = IntegerField()
     comment_date = TimestampField(default=datetime.datetime.now())
@@ -30,16 +18,12 @@ class comment(Model):
         database = database_connection.db_conn.db
         db_table = 'comment'
 
-    def save_orders(self):
+    def save_comment(self):
         with database_connection.db_conn.db:
-            print(self)
             if not self.table_exists():
                 database_connection.db_conn.db.create_tables([self])
-
             val.Validator.valid_comment = self
             val.Validator.validate()
             self.save()
             return 'ok'
-
-
 
