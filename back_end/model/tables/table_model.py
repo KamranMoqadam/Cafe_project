f0851639f8c3d7e89fd1f27e6d9c4c0ab8e82ab3
Ali <64635579+Ali-Mns-hub@users.noreply.github.com>
2022-09-17 19:43:14 +0430

    Update table_model.py
    Signed-off-by: Ali <64635579+Ali-Mns-hub@users.noreply.github.com>

--- back_end/model/tables/table_model.py
+++ back_end/model/tables/table_model.py
@@ -1,1 +1,31 @@
+from peewee import *
+import back_end.core.db_connection as database_connection
+import back_end.model.tables.utils as val
+
+
+db = PostgresqlDatabase('postgres', host='gina.iran.liara.ir', port=33704, user='root',
+                        password='nhmlvrFkHKBA5jxjb9rycf1w')
+
+class tables(Model):
+    table_id = AutoField()
+    table_number = IntegerField()
+    capacity = IntegerField()
+    booking = BooleanField()
+    
+    class Meta:
+        database = database_connection.db_conn.db
+        db_table = 'tables'
+        
+    def save_orders(self):
+        with database_connection.db_conn.db:
+            print(self)
+            if not self.table_exists():
+                database_connection.db_conn.db.create_tables([self])
+
+            val.Validator.valid_table = self
+            val.Validator.validate()
+            self.save()
+            return 'ok'
+
+
 
