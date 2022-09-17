6fdb536ac59993cd3e7687b7657cd23f0d5d4445
amjad ahmadi <amjadahmadi883@gmail.com>
2022-09-17 22:46:05 +0430

    updated

--- back_end/model/recipts/utils.py
+++ back_end/model/recipts/utils.py
@@ -1,22 +1,23 @@
-from back_end.model.recipts import recipts_model
-from ..log import cafe_log
+from back_end.model.log import cafe_log
+from back_end.model.users.users_model import Users
+from back_end.model.order.oredr_model import orders
+
 
 class Validator:
-    valid_receipt: recipts_model.receipts
+    valid_receipt: any
 
     @classmethod
     def validate(cls):
-        if isinstance(cls.valid_receipt.order_id, int):
+        if not isinstance(cls.valid_receipt.order_id, orders):
             cafe_log.error("invalid take order id")
             raise ValueError("invalid order id")
-        if isinstance(cls.valid_receipt.user_id, int):
+        if not isinstance(cls.valid_receipt.user_id, Users):
             cafe_log.error("invalid take user id")
             raise ValueError("invalid user id")
-        if isinstance(cls.valid_receipt.total_price, str):
+        if not isinstance(cls.valid_receipt.total_price, str):
             cafe_log.error("invalid take total price")
             raise ValueError("invalid total price")
-        if isinstance(cls.valid_receipt.final_price, str):
+        if not isinstance(cls.valid_receipt.final_price, str):
             cafe_log.error("invalid take final_price")
             raise ValueError("invalid final_price")
-    
-        
+
